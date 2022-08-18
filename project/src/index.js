d9c9be215c6f7285ff9c86876435e1b7ee3d11fd
taesubyun <taesubyun@icloud.com>
2022-08-18 14:05:50 +0900

    Set-up router

--- project/src/index.js
+++ project/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<Router>
+  <Routes>
+    <Route path='/' element={<App />} />
+  </Routes>
+</Router>;
+root.render();
